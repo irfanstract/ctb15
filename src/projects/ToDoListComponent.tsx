bebd15efc9f6327fe3cdc1e350d2db86dc7bac5f
meh <meh@meh>
2023-03-26 13:21:14 +0700

    update `src\projects\ToDoListComponent.tsx` like this

---
+++ src/projects/ToDoListComponent.tsx
@@ -0,0 +1,283 @@
+
+import * as util from "src/utility-functions/all" ;
+
+
+
+
+import {
+  CommiteeDesc ,
+  TaskDesc ,
+} from "src/projects/ToDoListing" ;
+
+import Button from "./Button";
+
+import TdlCss from "./TDL.module.css" ;
+
+const tlcExemplaryTasksList = (
+  [
+    {
+      title: `simple task 1A` ,
+      done: true ,
+    } ,
+    { 
+      assignees: [ 
+        { title: "Ve Rn" , } ,
+      ] ,
+    } ,
+    {
+      title: `Task 3` ,
+    } ,
+    {
+      title: `simple task 1C` ,
+      assignees: [
+        { title: "Ve Rb" , } ,
+        { title: "Ve Rn" , } ,
+        { } ,
+      ] ,
+    } ,
+    { 
+      title: `simple task 1D` ,
+      assignees: [ 
+        { title: "Ve Rn" , } ,
+      ] ,
+    } ,
+    {
+      title: `Task 6` ,
+    } ,
+  ] satisfies TaskDesc[]
+) ;
+
+const ToDoListComponent: (
+  util.React.FC<(
+    & { value: 3 | TaskDesc[] ; }
+  )>
+) = ({
+  value: tasks0 ,
+}) => {
+  const {
+    tasks ,
+  } = (
+    ((): (
+      & {
+        tasks: TaskDesc[] ;
+      }
+    ) => {
+      if (tasks0 === 3) {
+        return {
+          tasks: tlcExemplaryTasksList ,
+        } ;
+      }
+      return {
+        tasks: tasks0 ,
+      } ;
+    })() 
+  ) ;
+  return (
+    <div className={`${TdlCss.ToDoList } ` }> 
+      <p>
+        To-Do List
+      </p>
+      <ol>
+      {(
+        tasks
+        .map(desc => /* add UID */ ({ ...desc, uid: JSON.stringify(desc), }) ) 
+        .map((entry): util.React.ReactElement => {
+          const {
+            uid: tKey ,
+            title: entryTitle = "",
+            assignees: mAssignees = [] ,
+            done: mDone = false ,
+          } = entry ;
+          const titleDisplay = (
+            <p>
+              title: { getTitleUtfAsQuotedOrSayNoDesc(entryTitle) }
+            </p>
+          ) ;
+          const assignmentalDisplay = (
+            <div>
+            <p>
+              assignees:
+            </p>
+            <ul>
+            { (
+              mAssignees
+              .map(desc => /* add UID */ ({ ...desc, uid: JSON.stringify(desc), }) ) 
+              .map((entry) => {
+                const aKey = entry.uid ;
+                const aTitle = (
+                  getTitleUtfAsQuotedOrSayNoDesc(entry.title ?? "")
+                ) ;
+                return (
+                  <li key={aKey } >
+                    <a href="javascript:void(0)">
+                    { aTitle }
+                    </a>
+                  </li>
+                ) ;
+              })
+            ) }
+            </ul>
+            </div>
+          ) ;
+          const completionalStatDisplay = (
+            <PercentualCompletionalStatRender 
+            value={(mDone === true) ? 1 : 0.5 }
+            onMarkAsCompleted={(
+              (1 < mAssignees.length) ? false : (() => {})
+            )} // TODO
+            />
+          ) ;
+          return (
+            <li key={tKey} >
+              { titleDisplay }
+              { assignmentalDisplay }
+              { completionalStatDisplay }
+            </li>
+          ) ;
+        } )
+      )}
+      </ol>
+    </div>
+  ) ;
+} ;
+
+const PercentualCompletionalStatRender: (
+  util.React.FC<(
+    & { value: 0 | 0.5 | 1 ; }
+
+    & Partial<{ 
+      
+      /** 
+       * optional action to run whenever "marrk as completed" gets clicked.
+       * can be omitted, in which case would avoid rendering such a button.
+       * 
+       */
+      onMarkAsCompleted: false | util.React.DispatchWithoutAction ; 
+
+    }>
+
+  )>
+) = ({
+  value ,
+  onMarkAsCompleted: markAsCompleted = false ,
+}) => {
+  const statusDisplayParagraph = (
+    <p>
+      Status: {} 
+      {( 
+        { 
+          [ 0   ]: "0", 
+          [ 0.5 ]: "in progress", 
+          [ 1   ]: "completed",
+        }[value] 
+      )}
+    </p>
+  ) ;
+  const optionalMarkPretendCompleteButton = (
+    (value < 1) && (
+      markAsCompleted && (
+        <Button
+        onClick={() => markAsCompleted()}
+        >
+          Mark As Completed
+        </Button>
+      )
+    )
+  ) ;
+  const extraActionsBar = (
+    <p>
+      { optionalMarkPretendCompleteButton }
+    </p>
+  ) ;
+  return (
+    <div>
+      { statusDisplayParagraph }
+      { extraActionsBar }
+    </div>
+  ) ;
+} ;
+
+const getTitleUtfAsQuotedOrSayNoDesc: {
+  (v: string): util.React.ReactElement ;
+} = (
+  (desc0) => (
+    desc0 ?
+    <q>{ desc0 }</q>
+    : <span>(<em>no title</em>)</span>
+  )
+) ;
+
+
+
+
+export default ToDoListComponent ;
+
+export const ToDoListDemoComponent = ( 
+  () => {
+    const [value, setValue] = (
+      util.React.useState<(
+        util.React.ComponentProps<typeof ToDoListComponent>["value"] & {}[]
+      )>(tlcExemplaryTasksList)
+    ) ;
+    return (
+      <ToDoListComponent 
+      value={value}
+      />
+    ) ;
+  }
+) ;
+
+// assert they will all compile
+{
+  (
+    <Button type="button">
+        Do Some Action
+    </Button>
+  ) ;
+  <ToDoListDemoComponent /> ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {} ; // TS(1208)
