833b7c7887d3c8dfc15859b4c22a40cc915f6f58
meh <meh@meh>
2023-04-12 17:32:46 +0700

    fix the issues of `getScreenCTM` and `getCTM` of `SVGGraphicsElement` returning the incompatible interface

--- src/window-polyfills.tsx
+++ src/window-polyfills.tsx
@@ -8,6 +8,32 @@ import "core-js/proposals/map-upsert-stage-2" ;
 
 
 
+/* Blink/Chr Bug Fix */
+for (const methodName of ["getScreenCTM", "getCTM"] as const) {
+  const originalImpl = (
+    SVGGraphicsElement.prototype[methodName]
+  ) ;
+  SVGGraphicsElement.prototype[methodName] = (
+    function (this: SVGGraphicsElement, ...args ) {
+      const originalReturnVal = (
+        originalImpl.apply(this, ...args) as 
+        (SVGMatrix | DOMMatrix )
+      ) ;
+      const newReturnVal = (
+        // TODO
+        DOMMatrix.fromMatrix((
+          originalReturnVal
+        ))
+      ) satisfies DOMMatrix ;
+      return (
+        newReturnVal
+      ) ;
+    }
+  ) ;
+} ;
+
+
+
 
 
 export const FIVE: 5 = 5 ;
