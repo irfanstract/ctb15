fb43f0372e4590db65b637e28b0b3f3159c11852
meh <meh@meh>
2023-04-17 18:24:27 +0700

    `svp\data-model\interop-svg-paths-display` - `PathDSvEditComp` - `pointsListPlot` - add basic dragging support

--- src/projects/svp/data-model/interop-svg-paths-display.tsx
+++ src/projects/svp/data-model/interop-svg-paths-display.tsx
@@ -85,8 +85,53 @@ export const PathDSvEditComp = (
                       startDrag ,
                     }, i) => {
                     ;
+                    let currentDragging: null | ReturnType<(typeof startDrag) & {}> = null ;
                     return (
                       <Kk key={i}>
+                        <g
+                        // TODO
+                        /** 
+                         * remarks :
+                         * - Mouse Events only deals with "mouse"s ;
+                         *   Pointer Events is what deals with arbitrary "pointing device"
+                         * 
+                         */
+                        {...(
+                          startDrag ?
+                          {
+                            onPointerDown: evt => {
+                              console["log"](`PLPL evt coord`, evt.nativeEvent, (
+                                translateClientPos(evt.nativeEvent )
+                              )) ;
+                              currentDragging = (
+                                startDrag?.()
+                                ?? null
+                              ) ;
+                            } ,
+                            onPointerMove: evt => {
+                              if (currentDragging) {
+                                const evtLocalP = (
+                                  translateClientPos(evt.nativeEvent )
+                                ) ;
+                                currentDragging.moveTo({
+                                  newAbsolutePos: evtLocalP ,
+                                }) ;
+                              }
+                            } ,
+                            onPointerUp: () => {
+                              currentDragging?.close() ;
+                              currentDragging = null ;
+                            } ,
+
+                            style: {
+                              cursor: "move" ,
+                            } ,
+                          }
+                          : {
+                            style: {} ,
+                          }
+                        )}
+                        >
                         <rect 
                         x={p.x }
                         y={p.y }
@@ -94,6 +139,7 @@ export const PathDSvEditComp = (
                         height={lineStylingCssProps.strokeWidth ?? 0.3}
                         {...(ctrlType === "ctrl" ? { stroke: `rgb(128,0,128)`, } : {} )}
                         />
+                        </g>
                       </Kk>
                     ) ;
                     } )
