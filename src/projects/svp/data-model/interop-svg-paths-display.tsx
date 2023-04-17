8879f0ff42d69cf5223567c0d2b0b8ede8e662a8
meh <meh@meh>
2023-04-17 18:17:15 +0700

    `svp\data-model\interop-svg-paths-display` - `PathDSvEditComp` - `pointsListPlot` - add basic dragging support

--- src/projects/svp/data-model/interop-svg-paths-display.tsx
+++ src/projects/svp/data-model/interop-svg-paths-display.tsx
@@ -85,8 +85,41 @@ export const PathDSvEditComp = (
                       startDrag ,
                     }, i) => {
                     ;
+                    let currentDragging: null | ReturnType<(typeof startDrag) & {}> = null ;
                     return (
                       <Kk key={i}>
+                        <g
+                        // TODO
+                        /** 
+                         * remarks :
+                         * - Mouse Events only deals with "mouse"s ;
+                         *   Pointer Events is what deals with arbitrary "pointing device"
+                         * 
+                         */
+                        onPointerDown={evt => {
+                          console["log"](`PLPL evt coord`, evt.nativeEvent, (
+                            translateClientPos(evt.nativeEvent )
+                          )) ;
+                          currentDragging = (
+                            startDrag?.()
+                            ?? null
+                          ) ;
+                        }}
+                        onPointerMove={evt => {
+                          if (currentDragging) {
+                            const evtLocalP = (
+                              translateClientPos(evt.nativeEvent )
+                            ) ;
+                            currentDragging.moveTo({
+                              newAbsolutePos: evtLocalP ,
+                            }) ;
+                          }
+                        }}
+                        onPointerUp={() => {
+                          currentDragging?.close() ;
+                          currentDragging = null ;
+                        }}
+                        >
                         <rect 
                         x={p.x }
                         y={p.y }
@@ -94,6 +127,7 @@ export const PathDSvEditComp = (
                         height={lineStylingCssProps.strokeWidth ?? 0.3}
                         {...(ctrlType === "ctrl" ? { stroke: `rgb(128,0,128)`, } : {} )}
                         />
+                        </g>
                       </Kk>
                     ) ;
                     } )
